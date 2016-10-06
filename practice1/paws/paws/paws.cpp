8e22fee0a90de33e97ad0986f08af72cdcf2872c
AleciLimas <alecilimas@gmail.com>
2016-10-05 22:29:02 -0500

    added paws
    paws

---
+++ practice1/paws/paws/paws.cpp
@@ -0,0 +1,17 @@
+// paws.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	for (int i = 0; i < 10; i++) {
+		cout << i << endl;
+	}
+
+    return 0;
+}
+
